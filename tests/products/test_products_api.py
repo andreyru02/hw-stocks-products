dbb72a00b75dd2c119315fea3cf69ca3608f72f3
Andrey Chebunin <andrey.tchebunin@yandex.ru>
2021-09-15 21:30:53 +0800

    fix error

--- tests/products/test_products_api.py
+++ tests/products/test_products_api.py
@@ -1,6 +1,16 @@
 import pytest
-# from django.urls import reverse
+from django.urls import reverse
 
 
-def test_get_products():
+@pytest.mark.django_db
+def test_get_products(api_client, product_factory):
+    product = product_factory(_quantity=1)
+    url = reverse('product-detail', args=(product[0].id,))
+    resp = api_client.get(url)
+    resp_json = resp.json()
+
+    assert resp.status_code == 200
+    assert product[0].id == resp_json.get('id')
+    assert product[0].title == resp_json.get('title')
     assert 'Andrey' == 'Andrey'
+
